9460b47d3453b21376c60787ddaa5cd2c4bbe38f
Ganmo <m.kato1201@gmail.com>
2023-10-12 04:07:15 +0000

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |n|
+5.times do |n|
   User.create(
-    name: "testuser#{n+1}",
-    email: "testuser#{n+1}@test.com",
-    password: "testuser#{n+1}",
+    name: "user#{n+1}",
+    email: "test#{n+1}@test.com",
+    password: "123456",
   )
+  puts "User #{n+1} created"
 end
 
 
-# Userそれぞれに対して、10レコードずつBookを作成
-# 現在時刻から 0 ~ 15 までのランダムな数字を引いた時刻でBookを作成
-
 User.all.each do |user|
-  10.times do |n|
+  5.times do |n|
     Book.create(
-      title: "testbook#{n+1}",
-      body: "testbook#{n+1}",
+      title: "本#{n+1}",
+      body: "サンプル投稿です#{n+1}",
       user_id: user.id,
-      created_at: Time.current - rand(15).day
+      created_at: Time.current - rand(10).day
     )
+    puts "Book for User #{user.id} created"
   end
 end
